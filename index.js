40cb60ec9f3ab9c89faab96fbf9e42eb3d3e0525
Tom Conlon <tomconlonuk@gmail.com>
2018-10-02 16:57:58 +0100

    Update index.js
    pass event object to event handler if no function args provided in template

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 // Synthetic Events
-
 const nativeToSyntheticEvent = (event, name) => {
     const eventKey = `__${name}`
     let dom = event.target
+    let fnargs = `__${name}Data`    
     while(dom !== null) {
         const eventHandler = dom[eventKey]
         if (eventHandler) {
-            eventHandler(dom[`__${name}Data`])
+            // if no function args then call handler with the event object itself
+           if (dom[fnargs]){
+              eventHandler(dom[fnargs])
+           } 
+           else {
+              eventHandler(event)
+           }
             return
         }
         dom = dom.parentNode
